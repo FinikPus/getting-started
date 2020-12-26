95aaa5b2216bff0ab9a1507969eaeb81540c2864
FinikPus <finikmilki@yandex.ru>
2020-12-26 04:42:18 +0300

    add metrics

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,6 +1,7 @@
 package org.acme.getting.started;
 
 import org.jboss.logging.Logger;
+import io.prometheus.client.Counter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,12 +10,14 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
+    static final Counter requests = Counter.build()
+            .name("requests_total").help("Total requests.").register();
     private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        requests.inc();
         LOG.info("Hello");
         return "hello";
     }
